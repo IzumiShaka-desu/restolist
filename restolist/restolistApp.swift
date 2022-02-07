e6c1a4f3671ca2bb65e6680869db1cb4df89d9d1
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-07 20:15:41 +0700

    Initial Commit

---
+++ restolist/restolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  restolistApp.swift
+//  restolist
+//
+//  Created by Akashaka on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct restolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
